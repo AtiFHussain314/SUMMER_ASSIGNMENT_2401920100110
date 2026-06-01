7faca2476a735812c2ce9b424c12c3cc5f465b65
AtiFHussain314 <atifhussain4829@gmail.com>
2026-06-01 20:36:46 +0530

    Refactor maxProfit to use minPrice for calculation

--- Week_1/Day_1/121_Buy_and_Sell.cpp
+++ Week_1/Day_1/121_Buy_and_Sell.cpp
@@ -3,17 +3,14 @@ using namespace std ;
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int buyPrice = prices[0];
-        int profit = 0;
-
-        for (int i = 1; i < prices.size(); i++) {
-            if (buyPrice > prices[i]) {
-                buyPrice = prices[i];
-            }
-
-            profit = max(profit, prices[i] - buyPrice);
-        }
-
-        return profit;        
+    int minPrice = INT_MAX, maxProfit = 0;
+    for (int price : prices) {
+        minPrice = min(minPrice, price);
+        maxProfit = max(maxProfit, price - minPrice);
     }
+    return maxProfit;
+}
+
+    
 };
+
