61eb0cd20fa6e2d93d00bd7b034d31a63fe55382
AtiF Hussain <atifhussain4829@gmail.com>
2026-06-01 18:19:36 +0530

    Solution for Two sum

---
+++ Week1/Two_Sum.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std ;
+int main (){
+    class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> map;
+
+        for(int i=0;i<nums.size();i++){
+            int x = target - nums[i];
+
+           
+         if(map.count(x))
+         return {map[x], i};
+
+            map[nums[i]] = i;
+        }
+
+        return {};
+    }
+};
+}
