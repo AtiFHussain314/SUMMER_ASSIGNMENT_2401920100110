bca7e36fd2e330e9555fb790ff800ecc817511c2
AtiF Hussain <atifhussain4829@gmail.com>
2026-06-10 22:32:17 +0530

    solution for 28 and 392 of leetcode

---
+++ Week_2/Day_3/28_first_occurence.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std ;
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+         int n = haystack.length();
+        int m = needle.length();
+
+        for(int i = 0; i <= n - m; i++){
+            if(haystack.substr(i, m) == needle){
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
