3acea576208c027fddef8e7ead9f459fc9cea3ec
AtiFHussain314 <atifhussain4829@gmail.com>
2026-06-17 10:10:13 +0530

    Add string compression functionality
    Implement string compression algorithm to reduce consecutive characters.

---
+++ Week_2/Day_5/String_compression.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std ;
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        int n = chars.size();
+        int idx = 0;
+
+        for (int i = 0; i < n; ) {
+            char curr = chars[i];
+            int count = 0;
+
+            while (i < n && chars[i] == curr) {
+                count++;
+                i++;
+            }
+
+            chars[idx++] = curr;
+
+            if (count > 1) {
+                string cnt = to_string(count);
+                for (char c : cnt)
+                    chars[idx++] = c;
+            }
+        }
+
+        return idx;
+    }
+};
