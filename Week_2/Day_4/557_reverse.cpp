9c7212d2bef9f1d44607e3e743a37301a3f4b92f
AtiF Hussain <atifhussain4829@gmail.com>
2026-06-17 09:57:07 +0530

    solutions for "reverse string"

---
+++ Week_2/Day_4/557_reverse.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    string reverseWords(string s) {
+        stringstream ss(s);
+        string word, result = "";
+
+        while (ss >> word) {
+            reverse(word.begin(), word.end());
+            result += word + " ";
+        }
+
+        result.pop_back(); 
+        return result;
+    }
+};
